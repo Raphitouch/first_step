d8a6902fca4bb141415f24890e3b273bfc646a1b
tomerpq <tomerpatum@gmail.com>
2018-12-23 18:40:59 +0200

    update main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
-#include "Lexer.h"
+#include "Parser.h"
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "TOMER starts here : \n ******************************* \n" << std::endl;
-    Lexer* lexer = new Lexer(argv[1]);
-    string* s = lexer->lex();
-    for(int i = 0; i < lexer->getNum(); i++){
-        cout << "s[" << i << "] = " << s[i] << endl;
-    }
-    delete[] s;
+    Parser* parser = new Parser(new Lexer(argv[1]));
+    parser->parse();
     return 0;
 }
